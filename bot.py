017b94ef7044af622f0175ea4b64ccbf200b982b
appositum <eudes.scar@gmail.com>
2018-04-09 23:50:52 -0300

    Basic plot setup

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+from os import path
 
+import sympy as syp
 from discord.ext.commands import Bot
 
 
-bot = Bot(command_prefix="plot ")
+bot = Bot(command_prefix="p$")
 TOKEN = os.environ.get("TOKEN")
+
+if not path.exists('.log'): os.mkdir('.log')
+log_dir = path.abspath('.log')
 
 
 @bot.event
@@ -19,6 +24,17 @@ async def on_ready():
 @bot.command(aliases=['test'])
 async def hello(*args):
     return await bot.say('Hello world!')
+
+
+@bot.command(pass_context=True)
+async def plot(ctx, *, exp):
+    exp = syp.sympify(exp)
+
+    img_path = path.join(log_dir, 'figure.png')
+    img = syp.plot(exp, show=False)
+    img.save(img_path)
+
+    return await bot.upload(img_path)
 
 
 bot.run(TOKEN) if TOKEN else print('TOKEN environment variable not set!')
